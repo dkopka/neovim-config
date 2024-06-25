2df87dc4b11fdaf92da1cce47ea72bae67a0c29f
Dariusz Kopka <dariusz@plume.com>
2024-06-25 10:37:52 +0200

    fixed plug.lua (missing comma)

--- lua/plug.lua
+++ lua/plug.lua
@@ -1,6 +1,6 @@
 require('telescope').setup{}
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { "rust", "lua", "python" }
+    ensure_installed = { "rust", "lua", "python" },
     highlight = { enable = false },
 }
 require('lspconfig').rust_analyzer.setup{}
